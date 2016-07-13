77013af699de0410b77e566fdafa08cf63d76d25
Volkan Şahin <volkan.sahin@agem.com.tr>
2016-07-13 16:38:12 +0300

    adapted new modes

--- ahenk-wol/wol/safe.py
+++ ahenk-wol/wol/safe.py
@@ -6,9 +6,9 @@ from base.system.system import System
 
 
 class ManageWol(AbstractPlugin):
-    def __init__(self, username, context):
+    def __init__(self, context):
         super(ManageWol, self).__init__()
-        self.username = username
+        self.username = str(context.get_username())
         self.context = context
         self.logger = self.get_logger()
         self.connected_interfaces = System.Hardware.Network.interfaces()
@@ -16,11 +16,11 @@ class ManageWol(AbstractPlugin):
         self.logger.debug('[WOL - safe.py] Parameters were initialized.')
 
     def handle_safe_mode(self):
-
         for interface in self.connected_interfaces:
             self.logger.debug('[Wol - safe.py] Activating magic packet for ' + str(interface))
             self.execute('ethtool -s ' + str(interface) + ' wol g')
 
-def handle_safe_mode(username, context):
-    manage = ManageWol(username, context)
+
+def handle_mode(context):
+    manage = ManageWol(context)
     manage.handle_safe_mode()
