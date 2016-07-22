2f8a60f4a08648d34720c1257ac66506c8e6c09d
mine.dogan <mine.dogan@agem.com.tr>
2016-07-22 15:17:23 +0300

    code revision

--- ahenk-wol/wol/init.py
+++ ahenk-wol/wol/init.py
@@ -3,7 +3,6 @@
 # Author:Mine DOGAN <mine.dogan@agem.com.tr>
 
 from base.plugin.abstract_plugin import AbstractPlugin
-from base.system.system import System
 
 
 class Init(AbstractPlugin):
@@ -11,13 +10,12 @@ class Init(AbstractPlugin):
         super(Init, self).__init__()
         self.context = context
         self.logger = self.get_logger()
-        self.connected_interfaces = System.Hardware.Network.interfaces()
 
         self.logger.debug('[WOL - init] Parameters were initialized.')
 
     def handle_init_mode(self):
 
-        for interface in self.connected_interfaces:
+        for interface in self.Hardware.Network.interfaces():
             self.logger.debug('[WOL - init] Activating magic packet for ' + str(interface))
             self.execute('ethtool -s ' + str(interface) + ' wol g')
             self.logger.debug('[WOL - init] Activated magic packet for ' + str(interface))
--- ahenk-wol/wol/shutdown.py
+++ ahenk-wol/wol/shutdown.py
@@ -3,7 +3,6 @@
 # Author:Mine DOGAN <mine.dogan@agem.com.tr>
 
 from base.plugin.abstract_plugin import AbstractPlugin
-from base.system.system import System
 
 
 class Shutdown(AbstractPlugin):
@@ -11,13 +10,12 @@ class Shutdown(AbstractPlugin):
         super(Shutdown, self).__init__()
         self.context = context
         self.logger = self.get_logger()
-        self.connected_interfaces = System.Hardware.Network.interfaces()
 
         self.logger.debug('[WOL - shutdown] Parameters were initialized.')
 
     def handle_shutdown_mode(self):
 
-        for interface in self.connected_interfaces:
+        for interface in self.Hardware.Network.interfaces():
             self.logger.debug('[WOL - shutdown] Activating magic packet for ' + str(interface))
             self.execute('ethtool -s ' + str(interface) + ' wol g')
             self.logger.debug('[WOL - shutdown] Activated magic packet for ' + str(interface))
