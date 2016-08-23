fa4012fe41041b041d852172f32c6f499865786a
emrekgn <emre.akkaya@agem.com.tr>
2016-08-23 18:38:09 +0300

    use unsigned changes file

--- scripts/build-plugin.sh
+++ scripts/build-plugin.sh
@@ -21,7 +21,7 @@ echo "lider & lider-console modules built successfully."
 # Generate Ahenk package
 echo "Generating Ahenk package..."
 cd "$PRJ_ROOT_PATH"/ahenk-wol
-dpkg-buildpackage -b
+dpkg-buildpackage -b -uc
 echo "Generated Ahenk package"
 
 EXPORT_PATH=/tmp/lider-ahenk-wol-plugin
