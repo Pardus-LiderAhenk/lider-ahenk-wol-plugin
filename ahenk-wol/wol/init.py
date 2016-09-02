747c1525a018db1daafb6d4bd4fb7d5726bf8a2d
mine.dogan <mine.dogan@agem.com.tr>
2016-09-02 18:11:04 +0300

    check installation at init

--- ahenk-wol/wol/init.py
+++ ahenk-wol/wol/init.py
@@ -15,10 +15,14 @@ class Init(AbstractPlugin):
 
     def handle_init_mode(self):
 
+        if self.is_installed('wakeonlan') == False:
+            self.logger.debug('[WOL-INIT] Installing wakeonlan with apt-get...')
+            self.install_with_apt_get('wakeonlan')
+
         for interface in self.Hardware.Network.interfaces():
-            self.logger.debug('[WOL - init] Activating magic packet for ' + str(interface))
+            self.logger.debug('[WOL-INIT] Activating magic packet for ' + str(interface))
             self.execute('ethtool -s ' + str(interface) + ' wol g')
-            self.logger.debug('[WOL - init] Activated magic packet for ' + str(interface))
+            self.logger.debug('[WOL-INIT] Activated magic packet for ' + str(interface))
 
 
 def handle_mode(context):
